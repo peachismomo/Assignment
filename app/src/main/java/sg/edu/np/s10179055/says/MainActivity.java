cba280bf429e65996506c515655c7182ad61b303
Peachismomo <s10179055@connect.np.edu.sg>
2019-07-03 17:08:19 +0800

    full file commit

---
+++ app/src/main/java/sg/edu/np/s10179055/says/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.s10179055.says;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//sd
