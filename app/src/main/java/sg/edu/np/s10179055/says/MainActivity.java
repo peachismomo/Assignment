990c8180f851faf7107c14fee5ebffe7293c4fe4
Peachismomo <s10179055@connect.np.edu.sg>
2019-07-04 19:12:01 +0800

    login and guest page

--- app/src/main/java/sg/edu/np/s10179055/says/MainActivity.java
+++ app/src/main/java/sg/edu/np/s10179055/says/MainActivity.java
@@ -1,7 +1,9 @@
 package sg.edu.np.s10179055.says;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onLogin(View v){
+        Intent myIntent = new Intent(getBaseContext(), login.class);
+        startActivity(myIntent);
+    }
+
+    public void onGuest(View v){
+        Intent myIntent = new Intent(getBaseContext(), guest.class);
+        startActivity(myIntent);
+    }
+
 }
-//sdallah
-//meme
-//jordan has double standards
