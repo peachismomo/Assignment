fb8a7f875602a1a6f6b73ff5a88cd1a1ea6e8983
Peachismomo <s10179055@connect.np.edu.sg>
2019-07-09 13:54:12 +0800

    login class

---
+++ app/src/main/java/sg/edu/np/s10179055/says/loginDetails.java
@@ -0,0 +1,36 @@
+package sg.edu.np.s10179055.says;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class loginDetails {
+    String username;
+    String password;
+
+    public void loginDetails(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean regexUsername() {
+        Pattern patternUser = Pattern.compile("^[A-Za-z]{6,12}$");
+        Matcher matcherUser = patternUser.matcher(username);
+        if (matcherUser.matches()) {
+            return true;
+        } else return false;
+    }
+
+    public boolean regexPassword() {
+        Pattern patternPass = Pattern.compile("^+[$&+,:;=?@#|]+[A-Z]+[0-9]$");
+        Matcher matcherPassword = patternPass.matcher(password);
+        if (matcherPassword.matches()) {
+            return true;
+        } else return false;
+    }
+
+    public boolean regex() {
+        if (regexUsername() && regexPassword()) {
+            return true;
+        } else return false;
+    }
+}
