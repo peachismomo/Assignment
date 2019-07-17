cc8a61dabd0ea6a1b1d7dbdd2fe8dc8b0ecc488c
Han Kai Xiang <s10170577@connect.np.edu.sg>
2019-07-17 22:22:20 +0800

    Updated fetchData class and foodPlacesFragmen
    Signed-off-by: Han Kai Xiang <s10170577@connect.np.edu.sg>

--- app/src/main/java/sg/edu/np/s10179055/says/fetchData.java
+++ app/src/main/java/sg/edu/np/s10179055/says/fetchData.java
@@ -2,15 +2,42 @@ package sg.edu.np.s10179055.says;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class fetchData extends AsyncTask<Void,Void,Void> {
+    String data;
     @Override
     protected Void doInBackground(Void... voids) {
-        String url1 = "https://www1.np.edu.sg/npnet/wifiatcanteen/CMXService.asmx/getChartData?Location=SystemCampus>Blk 51>Level2-Canteen>CoverageArea-B51L02";
-        URL url = new URL(url1);
-        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        try {
+            //will repeat code for all 4 and parse through to extract number
+            URL url = new URL("https://www1.np.edu.sg/npnet/wifiatcanteen/CMXService.asmx/getChartData?Location=SystemCampus>Blk 51>Level2-Canteen>CoverageArea-B51L02");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while (line != null){
+                line = bufferedReader.readLine();
+                data += line;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        foodPlacesFragment.tvDisplay.setText(this.data);
     }
 }
--- app/src/main/java/sg/edu/np/s10179055/says/foodPlacesFragment.java
+++ app/src/main/java/sg/edu/np/s10179055/says/foodPlacesFragment.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class foodPlacesFragment extends Fragment {
-
+    public static TextView tvDisplay;
+    Button btn;
 
     public foodPlacesFragment() {
         // Required empty public constructor
@@ -27,7 +30,15 @@ public class foodPlacesFragment extends Fragment {
         /*
         Code your spaghetti here
         */
+        tvDisplay = RootView.findViewById(R.id.textViewDisplay);
+        btn = RootView.findViewById(R.id.button);
+
         return RootView;
+    }
+
+    public void onClick(View view){
+        fetchData process = new fetchData();
+        process.execute();
     }
 
 }
