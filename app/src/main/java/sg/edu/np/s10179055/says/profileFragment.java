c2231f51eb7b050f0ec65f703459bd1ff7bde221
dreaz <s10179869@connect.np.edu.sg>
2019-07-23 12:33:59 +0800

    Error linking file resources

--- app/src/main/java/sg/edu/np/s10179055/says/profileFragment.java
+++ app/src/main/java/sg/edu/np/s10179055/says/profileFragment.java
@@ -2,18 +2,27 @@ package sg.edu.np.s10179055.says;
 
 
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class profileFragment extends Fragment {
-
-
+    private EditText n;
+    private DatabaseReference r;
     public profileFragment() {
         // Required empty public constructor
     }
@@ -22,14 +31,30 @@ public class profileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+            // Inflate the layout for this fragment
+            View RootView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+            n = RootView.findViewById(R.id.etName);
+            r = FirebaseDatabase.getInstance().getReference().child("Member");
+
+            r.addValueEventListener(new ValueEventListener()
+            {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+                {
+                    String name = dataSnapshot.child("Name").getValue().toString();
+                    n.setText(name);
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError)
+                {
+
+                }
+            });
+            return RootView;
     }
 
-    public void onClick(View v)
-    {
 
-    }
 
 }
